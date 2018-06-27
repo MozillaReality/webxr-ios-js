572eb572e57c423f7a1a11ecb8dd3b4289c85095
Trevor F. Smith <trevor@trevor.smith.name>
2018-06-27 14:37:21 -0700

    Remove old polyfill in preparation for IW CG polyfill

--- src/webxr/XRLayer.js
+++
@@ -1,8 +0,0 @@
-import EventHandlerBase from '../event/EventHandlerBase.js'
-
-/*
-XRLayer defines a source of bitmap images and a description of how the image is to be rendered in the XRDevice
-*/
-export default class XRLayer extends EventHandlerBase {
-	// Everything is implemented on XRWebGLLayer
-}
