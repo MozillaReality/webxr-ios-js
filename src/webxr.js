93603c7ab7af33d51e2e92ae1fabc87a224dd218
Trevor F. Smith <trevor@trevor.smith.name>
2018-08-08 14:49:43 -0700

    Added logging for debugging the hit transform issues.

--- src/webxr.js
+++ src/webxr.js
@@ -9,7 +9,7 @@ import XRHitResult from './extensions/XRHitResult.js'
 import ARKitDevice from './arkit/ARKitDevice.js'
 import ARKitWrapper from './arkit/ARKitWrapper.js'
 
-const workingMatrix = mat4.create()
+const _workingMatrix = mat4.create()
 
 // Monkey patch the WebXR polyfill so that it only loads our special XRDevice
 WebXRPolyfill.prototype._patchRequestDevice = function(){
@@ -32,17 +32,21 @@ Now install a few proposed AR extensions to the WebXR Device API:
 - anchors: https://github.com/immersive-web/anchors
 */
 
-function xrFrameOfReferenceGetTransformTo(otherFoR){
-	mat4.invert(workingMatrix, otherFoR[PRIVATE].transform)
-	let out = mat4.identity(mat4.create())
-	mat4.multiply(out, workingMatrix, out)
-	return mat4.multiply(out, this[PRIVATE].transform, out)
+function _xrFrameOfReferenceGetTransformTo(otherFoR){
+	return _getTransformTo(this[PRIVATE].transform, otherFoR[PRIVATE].transform)
 }
 
-const arKitWrapper = ARKitWrapper.GetOrCreate()
+function _getTransformTo(sourceMatrix, destinationMatrix){
+	mat4.invert(_workingMatrix, destinationMatrix)
+	let out = mat4.identity(mat4.create())
+	mat4.multiply(out, _workingMatrix, out)
+	return mat4.multiply(out, sourceMatrix, out)
+}
+
+const _arKitWrapper = ARKitWrapper.GetOrCreate()
 
 // This will be XRSession.requestHitTest
-async function xrSessionRequestHitTest(origin, direction, coordinateSystem) {
+async function _xrSessionRequestHitTest(origin, direction, coordinateSystem) {
 	// Promise<FrozenArray<XRHitResult>> requestHitTest(Float32Array origin, Float32Array direction, XRCoordinateSystem coordinateSystem);
 
 	// ARKit only handles hit testing from the screen, so only head model FoR is accepted
@@ -52,14 +56,21 @@ async function xrSessionRequestHitTest(origin, direction, coordinateSystem) {
 	return new Promise((resolve, reject) => {
 		// TODO get the actual near plane and FOV
 		// Calculate the screen coordinates from the origin
-		const normalizedScreenCoordinates = convertRayOriginToScreenCoordinates(origin, 0.1, 0.7853981633974483)
+		const normalizedScreenCoordinates = _convertRayOriginToScreenCoordinates(origin, 0.1, 0.7853981633974483)
+		console.log('and back', ...normalizedScreenCoordinates)
+
 		// Perform the hit test
-		arKitWrapper.hitTest(...normalizedScreenCoordinates, ARKitWrapper.HIT_TEST_TYPE_EXISTING_PLANES).then(hits => {
+		_arKitWrapper.hitTest(...normalizedScreenCoordinates, ARKitWrapper.HIT_TEST_TYPE_EXISTING_PLANES).then(hits => {
+			if(hits.length === 0) resolve([])
 			// Hit results are in the tracker (aka eye-level) coordinate system, so transform them back to head-model since the passed origin and results must be in the same coordinate system
 			this.requestFrameOfReference('eye-level').then(eyeLevelFrameOfReference => {
 				const csTransform = eyeLevelFrameOfReference.getTransformTo(coordinateSystem)
+				console.log('eye to head', mat4.getTranslation(vec3.create(), csTransform), mat4.getRotation(new Float32Array(4), csTransform))
 				resolve(hits.map(hit => {
-					return new XRHitResult(mat4.multiply(mat4.create(), hit.world_transform, csTransform))
+					const hitInHeadMatrix = mat4.multiply(mat4.create(), hit.world_transform, csTransform)
+					console.log('world transform', mat4.getTranslation(vec3.create(), hit.world_transform), mat4.getRotation(new Float32Array(4), hit.world_transform))
+					console.log('head transform', mat4.getTranslation(vec3.create(), hitInHeadMatrix), mat4.getRotation(new Float32Array(4), hitInHeadMatrix))
+					return new XRHitResult(hitInHeadMatrix)
 				}))
 			})
 		}).catch((...params) => {
@@ -74,7 +85,7 @@ Take a vec3 point on the screen in world space and return normalized x,y screen 
 @param rayOrigin {vec3}
 @return [x,y] in range [-1,1]
 */
-function convertRayOriginToScreenCoordinates(rayOrigin, near, fov){
+function _convertRayOriginToScreenCoordinates(rayOrigin, near, fov){
 	const nearLengthFromCenter = near * Math.tan(fov / 2)
 	let x = rayOrigin[0] / nearLengthFromCenter
 	let y = rayOrigin[1] / nearLengthFromCenter
@@ -90,17 +101,17 @@ function convertRayOriginToScreenCoordinates(rayOrigin, near, fov){
 	return [x, y]
 }
 
-function installExtensions(){
+function _installExtensions(){
 	if(!navigator.xr) return
 
 	if(window.XRSession){
-		XRSession.prototype.requestHitTest = xrSessionRequestHitTest
+		XRSession.prototype.requestHitTest = _xrSessionRequestHitTest
 	}
 	if(window.XRFrameOfReference){
-		XRFrameOfReference.prototype.getTransformTo = xrFrameOfReferenceGetTransformTo
+		XRFrameOfReference.prototype.getTransformTo = _xrFrameOfReferenceGetTransformTo
 	}
 }
 
-installExtensions()
+_installExtensions()
 
 
