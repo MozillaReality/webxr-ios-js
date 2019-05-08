40cfef997538761f84de4e0f472346b7936aa99e
Blair MacIntyre <bmacintyre@mozilla.com>
2019-05-08 16:27:43 -0400

    Update XRInputManager.js

--- examples/XRInputManager.js
+++ examples/XRInputManager.js
@@ -1,5 +1,5 @@
-import * as mat4 from '/test/libs/gl-matrix/mat4.js'
-import * as vec3 from '/test/libs/gl-matrix/vec3.js'
+import * as mat4 from 'gl-matrix/src/gl-matrix/mat4'
+import * as vec3 from 'gl-matrix/src/gl-matrix/vec3'
 
 const _workingMatrix1 = mat4.create()
 const _workingMatrix2 = mat4.create()
